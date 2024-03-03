f0f1430929e3da9ea208de70f375ee9bac023af1
Vulkii <Vulkii@yandex.ru>
2024-03-03 16:14:47 +0300

    Tags working api

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -16,3 +16,10 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
+
+
+class AllowAnyOrIsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST' or 'PUT' or 'PATCH':
+            return True
+        return request.method in ('GET', 'HEAD', 'OPTIONS') or request.user and request.user.is_staff
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from recipes.models import Tag
+from .permissions import IsAdminOrReadOnly, AllowAnyOrIsAdminOrReadOnly
+from .serializers import TagSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAnyOrIsAdminOrReadOnly,)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        return Response({"detail": "Method Not Allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response({"detail": "Method Not Allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response({"detail": "Method Not Allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)

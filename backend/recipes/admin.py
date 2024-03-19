b8922d5ac2f5606b8bbc21faf29bfa587d13d991
Vulkii <Vulkii@yandex.ru>
2024-03-19 11:27:58 +0300

    Added admin

---
+++ backend/recipes/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import (Tag, Ingredient, Recipe, IngredientInRecipe,
+                     Favourite, ShoppingCart)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'color', 'slug']
+    search_fields = ['name', 'slug']
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'measurement_unit']
+    search_fields = ['name']
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'author', 'cooking_time']
+    search_fields = ['name', 'author__username', 'ingredients__name']
+    list_filter = ['tags']
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ['recipe', 'ingredient', 'amount']
+    search_fields = ['recipe__name', 'ingredient__name']
+
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ['user', 'recipe']
+    search_fields = ['user__username', 'recipe__name']
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'recipe']
+    search_fields = ['user__username', 'recipe__name']
---
+++ backend/users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import User, Subscription
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name']
+    search_fields = ['username', 'email']
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'author']
+    search_fields = ['user__username', 'author__username']

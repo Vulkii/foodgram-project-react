86086054ab3ee03438598946d95ddbb5e4bd6f93
Vulkii <Vulkii@yandex.ru>
2024-03-19 11:30:56 +0300

    admin isort fix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import (Tag, Ingredient, Recipe, IngredientInRecipe,
-                     Favourite, ShoppingCart)
+
+from .models import (Favourite, Ingredient, IngredientInRecipe, Recipe,
+                     ShoppingCart, Tag)
 
 
 @admin.register(Tag)
